920442e9364d731aa71170064b65def7d5c6a72e
Raghav Sharma <raghav_ftw@hotmail.com>
2016-01-01 16:17:10 +0530

    Show canvas animation only if window width is greater than 991px

--- src/main/webapp/js/canvas.js
+++ src/main/webapp/js/canvas.js
@@ -1,28 +1,11 @@
-(function() {
-    var lastTime = 0;
-    var vendors = ['ms', 'moz', 'webkit', 'o'];
-    for(var x = 0; x < vendors.length && !window.requestAnimationFrame; ++x) {
-        window.requestAnimationFrame = window[vendors[x]+'RequestAnimationFrame'];
-        window.cancelAnimationFrame = window[vendors[x]+'CancelAnimationFrame']
-            || window[vendors[x]+'CancelRequestAnimationFrame'];
+$(document).ready(function () {
+
+    if ($(window).width() > 991) {
+        showCanvasAnimation();
     }
+});
 
-    if (!window.requestAnimationFrame)
-        window.requestAnimationFrame = function(callback, element) {
-            var currTime = new Date().getTime();
-            var timeToCall = Math.max(0, 16 - (currTime - lastTime));
-            var id = window.setTimeout(function() { callback(currTime + timeToCall); },
-                timeToCall);
-            lastTime = currTime + timeToCall;
-            return id;
-        };
-
-    if (!window.cancelAnimationFrame)
-        window.cancelAnimationFrame = function(id) {
-            clearTimeout(id);
-        };
-}());
-$(function() {
+function showCanvasAnimation() {
 
     var width, height, canvas, ctx, points, target, animateHeader = true;
     // Main
@@ -38,15 +21,15 @@ $(function() {
             y: height / 3
         };
 
-        canvas = document.getElementById( 'canvas' );
+        canvas = document.getElementById('canvas');
         canvas.width = width;
         canvas.height = height;
-        ctx = canvas.getContext( '2d' );
+        ctx = canvas.getContext('2d');
 
         // create points
         points = [];
-        for ( var x = 0; x < width; x = x + width / 20 ) {
-            for ( var y = 0; y < height; y = y + height / 20 ) {
+        for (var x = 0; x < width; x = x + width / 20) {
+            for (var y = 0; y < height; y = y + height / 20) {
                 var px = x + Math.random() * width / 20;
                 var py = y + Math.random() * height / 20;
                 var p = {
@@ -55,31 +38,31 @@ $(function() {
                     y: py,
                     originY: py
                 };
-                points.push( p );
+                points.push(p);
             }
         }
 
         // for each point find the 5 closest points
-        for ( var i = 0; i < points.length; i++ ) {
+        for (var i = 0; i < points.length; i++) {
             var closest = [];
-            var p1 = points[ i ];
-            for ( var j = 0; j < points.length; j++ ) {
-                var p2 = points[ j ]
-                if ( !( p1 == p2 ) ) {
+            var p1 = points[i];
+            for (var j = 0; j < points.length; j++) {
+                var p2 = points[j]
+                if (!( p1 == p2 )) {
                     var placed = false;
-                    for ( var k = 0; k < 5; k++ ) {
-                        if ( !placed ) {
-                            if ( closest[ k ] == undefined ) {
-                                closest[ k ] = p2;
+                    for (var k = 0; k < 5; k++) {
+                        if (!placed) {
+                            if (closest[k] == undefined) {
+                                closest[k] = p2;
                                 placed = true;
                             }
                         }
                     }
 
-                    for ( var k = 0; k < 5; k++ ) {
-                        if ( !placed ) {
-                            if ( getDistance( p1, p2 ) < getDistance( p1, closest[ k ] ) ) {
-                                closest[ k ] = p2;
+                    for (var k = 0; k < 5; k++) {
+                        if (!placed) {
+                            if (getDistance(p1, p2) < getDistance(p1, closest[k])) {
+                                closest[k] = p2;
                                 placed = true;
                             }
                         }
@@ -90,27 +73,27 @@ $(function() {
         }
 
         // assign a circle to each point
-        for ( var i in points ) {
-            var c = new Circle( points[ i ], 2 + Math.random() * 2, 'rgba(255,255,255,0.3)' );
-            points[ i ].circle = c;
+        for (var i in points) {
+            var c = new Circle(points[i], 2 + Math.random() * 2, 'rgba(255,255,255,0.3)');
+            points[i].circle = c;
         }
     }
 
     // Event handling
     function addListeners() {
-        if ( !( 'ontouchstart' in window ) ) {
-            window.addEventListener( 'mousemove', mouseMove );
+        if (!( 'ontouchstart' in window )) {
+            window.addEventListener('mousemove', mouseMove);
         }
-        window.addEventListener( 'scroll', scrollCheck );
-        window.addEventListener( 'resize', resize );
+        window.addEventListener('scroll', scrollCheck);
+        window.addEventListener('resize', resize);
     }
 
-    function mouseMove( e ) {
+    function mouseMove(e) {
         var posx = posy = 0;
-        if ( e.pageX || e.pageY ) {
+        if (e.pageX || e.pageY) {
             posx = e.pageX;
             posy = e.pageY;
-        } else if ( e.clientX || e.clientY ) {
+        } else if (e.clientX || e.clientY) {
             posx = e.clientX + document.body.scrollLeft + document.documentElement.scrollLeft;
             posy = e.clientY + document.body.scrollTop + document.documentElement.scrollTop;
         }
@@ -119,7 +102,7 @@ $(function() {
     }
 
     function scrollCheck() {
-        if ( document.body.scrollTop > height )
+        if (document.body.scrollTop > height)
             animateHeader = false;
         else
             animateHeader = true;
@@ -135,35 +118,35 @@ $(function() {
     // animation
     function initAnimation() {
         animate();
-        for ( var i in points ) {
-            shiftPoint( points[ i ] );
+        for (var i in points) {
+            shiftPoint(points[i]);
         }
     }
 
     function animate() {
-        if ( animateHeader ) {
-            ctx.clearRect( 0, 0, width, height );
-            for ( var i in points ) {
+        if (animateHeader) {
+            ctx.clearRect(0, 0, width, height);
+            for (var i in points) {
                 // detect points in range
-                if ( Math.abs( getDistance( target, points[ i ] ) ) < 4000 ) {
-                    points[ i ].active = 0.3;
-                    points[ i ].circle.active = 0.6;
-                } else if ( Math.abs( getDistance( target, points[ i ] ) ) < 20000 ) {
-                    points[ i ].active = 0.1;
-                    points[ i ].circle.active = 0.3;
-                } else if ( Math.abs( getDistance( target, points[ i ] ) ) < 40000 ) {
-                    points[ i ].active = 0.02;
-                    points[ i ].circle.active = 0.1;
+                if (Math.abs(getDistance(target, points[i])) < 4000) {
+                    points[i].active = 0.3;
+                    points[i].circle.active = 0.6;
+                } else if (Math.abs(getDistance(target, points[i])) < 20000) {
+                    points[i].active = 0.1;
+                    points[i].circle.active = 0.3;
+                } else if (Math.abs(getDistance(target, points[i])) < 40000) {
+                    points[i].active = 0.02;
+                    points[i].circle.active = 0.1;
                 } else {
-                    points[ i ].active = 0;
-                    points[ i ].circle.active = 0;
+                    points[i].active = 0;
+                    points[i].circle.active = 0;
                 }
 
-                drawLines( points[ i ] );
-                points[ i ].circle.draw();
+                drawLines(points[i]);
+                points[i].circle.draw();
             }
         }
-        requestAnimationFrame( animate );
+        requestAnimationFrame(animate);
     }
 
     function shiftPoint(p) {
@@ -177,41 +160,41 @@ $(function() {
     }
 
     // Canvas manipulation
-    function drawLines( p ) {
-        if ( !p.active )
+    function drawLines(p) {
+        if (!p.active)
             return;
-        for ( var i in p.closest ) {
+        for (var i in p.closest) {
             ctx.beginPath();
-            ctx.moveTo( p.x, p.y );
-            ctx.lineTo( p.closest[ i ].x, p.closest[ i ].y );
+            ctx.moveTo(p.x, p.y);
+            ctx.lineTo(p.closest[i].x, p.closest[i].y);
             ctx.strokeStyle = 'rgba(255,255,255,' + p.active + ')';
             ctx.stroke();
         }
     }
 
-    function Circle( pos, rad, color ) {
+    function Circle(pos, rad, color) {
         var _this = this;
 
         // constructor
-        ( function() {
+        (function () {
             _this.pos = pos || null;
             _this.radius = rad || null;
             _this.color = color || null;
         })();
 
-        this.draw = function() {
-            if ( !_this.active )
+        this.draw = function () {
+            if (!_this.active)
                 return;
             ctx.beginPath();
-            ctx.arc( _this.pos.x, _this.pos.y, _this.radius, 0, 2 * Math.PI, false );
+            ctx.arc(_this.pos.x, _this.pos.y, _this.radius, 0, 2 * Math.PI, false);
             ctx.fillStyle = 'rgba(255,255,255,' + _this.active + ')';
             ctx.fill();
         };
     }
 
     // Util
-    function getDistance( p1, p2 ) {
-        return Math.pow( p1.x - p2.x, 2 ) + Math.pow( p1.y - p2.y, 2 );
+    function getDistance(p1, p2) {
+        return Math.pow(p1.x - p2.x, 2) + Math.pow(p1.y - p2.y, 2);
     }
 
-});
+}
