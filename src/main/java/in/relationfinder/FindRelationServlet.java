8a066cf86c5470b2252dbd817bbcf6334b9f97e1
Raghav Sharma <raghav_ftw@hotmail.com>
2016-01-02 23:46:17 +0530

    Added some example queries to Init Parameter of the servlet. Also moved servlet mapping and other servlet meta stuff to deployment descriptor.

--- src/main/java/in/relationfinder/FindRelationServlet.java
+++ src/main/java/in/relationfinder/FindRelationServlet.java
@@ -3,24 +3,24 @@ package in.relationfinder;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 /**
- * Gets the search query, finds relation and renders it to user.
+ * Gets the search query, finds relation and dispatches request to jsp.
  *
  * @author Raghav
  */
-@WebServlet(name = "FindRelationServlet", urlPatterns = {"/find"})
 public class FindRelationServlet extends HttpServlet {
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         String raw_query = request.getParameter("query"); //Ex: father's mother's daughter
-
+        if (raw_query == null || raw_query.isEmpty())
+            raw_query = getRandomQuery(getServletConfig().getInitParameter("query"));
         List<String> relations = RelationsHandler.getRelation(raw_query);
 
         List<String[]> results = null;
@@ -39,6 +39,21 @@ public class FindRelationServlet extends HttpServlet {
         request.setAttribute("query", raw_query);
         RequestDispatcher requestDispatcher = request.getRequestDispatcher("index.jsp");
         requestDispatcher.forward(request, response);
+    }
+
+    /**
+     * Gets random query from Init Parameter. You can only store one value per parameter.
+     * So we seprate our queries by newline and store it all in one string. We then split it and get any random query.
+     *
+     * @param initParameter Value of the Init parameter.
+     * @return Any random query.
+     */
+    private String getRandomQuery(String initParameter) {
+        String[] queries = initParameter.split("\n");
+        String randomQuery = queries[new Random().nextInt(queries.length)];
+        randomQuery = randomQuery.trim();
+        return randomQuery;
+
     }
 
 }
